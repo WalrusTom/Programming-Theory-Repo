0860d47a4ce43495d1518629c02498149852fb45
WalrusTom <39208962+WalrusTom@users.noreply.github.com>
2021-08-22 16:33:11 +1000

    Animal-parent
    Animal parent class is finished and applied to squirrel in game.

---
+++ Programming Theory Project/Assets/Scripts/AnimalController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AnimalController : MonoBehaviour
+{
+    [SerializeField] protected bool movingRight = true;
+    [SerializeField] protected Rigidbody2D animalRb;
+    [SerializeField] public float jumpForce;
+    [SerializeField] public int runSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAnimal();
+        CheckInput();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (transform.position.y < -1.2)
+            {
+                Jump();
+            }
+            
+        }
+    }
+
+    protected virtual void Jump()
+    {
+        animalRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        
+    }
+
+    void MoveAnimal()
+    {
+        if (transform.position.x > 5)
+        {
+            movingRight = false;
+        }
+        if (transform.position.x < -5)
+        {
+            movingRight = true;
+        }
+
+        if (movingRight)
+        {
+            animalRb.transform.Translate(Vector2.right * runSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animalRb.transform.Translate(Vector2.left * runSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Programming Theory Project/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameplayManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this should work once we load this scene from the main menu scene
+        //nameDisplay.text = GameManager.instance.charname;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
