6b885a0db8995ac32b6002a5280bb2bafc164802
WalrusTom <39208962+WalrusTom@users.noreply.github.com>
2021-08-22 10:09:36 +1000

    Active buttons set up
    Have added selection for the buttons on the main menu

---
+++ Programming Theory Project/Assets/Scripts/AnimalSelectButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalSelectButton : MonoBehaviour
+{
+    public bool isSelected;
+    public List<AnimalSelectButton> sameListButtons;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSelected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClicked()
+    {
+        foreach (AnimalSelectButton button in sameListButtons)
+        {
+            button.isSelected = false;
+            button.GetComponent<Outline>().enabled = false;
+        }
+
+        isSelected = true;
+        GetComponent<Outline>().enabled = true;
+    }
+}
