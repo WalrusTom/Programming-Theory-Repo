d6758ed917f49d6bd3f9e4d2d048a6cc700753e1
WalrusTom <39208962+WalrusTom@users.noreply.github.com>
2021-08-22 12:27:57 +1000

    Finished title screen
    All title screen functionality done (except parts that interact with actual game)

---
+++ Programming Theory Project/Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartGameButton : MonoBehaviour
+{
+    public GameManager gameManager;
+    public TextMeshProUGUI nameInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        gameManager.charname = nameInput.text;
+    }
+}
